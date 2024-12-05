36712855db626ca8b842c2459f3117a6d8c2b0d2
Jace Van Oss <j-vanoss@onu.edu>
2024-12-05 08:27:39 -0500

    completed pressEnterToContinue

--- Lab13P13A-1.cpp
+++ Lab13P13A-1.cpp
@@ -1,1 +1,47 @@
- 
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <iomanip>
+
+using namespace std;
+
+void pressEnterToConinue(void);
+
+int main() {
+
+	string fileName;
+	cout << "Enter filename: ";
+	cin >> fileName;
+	ifstream file(fileName);
+	if (!file) {
+		cout << "Error opening the file" << endl;
+		return 1;
+	}
+	int letterCount[26] = { 0 };
+	int totalLetters = 0;
+	char letter;
+	while (file.get(letter)) {
+		if (isalpha(letter)) {
+			letter = tolower(letter);
+			letterCount[letter - 'a']++;
+			totalLetters++;
+		}
+	}
+	cout << "Total number of letters: " << totalLetters << endl;
+	cout << "Frequency: " << endl;
+	for (int i = 0; i < 26; i++) {
+		double frequency = (double)letterCount[i] / totalLetters * 100;
+		cout << "'" << (char)('A' + i) << "': " << fixed << setprecision(3) << frequency << endl;
+	}
+	pressEnterToConinue();
+
+	return 0;
+}
+// used to keep the console window open long enough to see your programs output
+void pressEnterToConinue(void) {
+	char c;
+	cout << "Press Enter to continue...";
+	cin.ignore(1024, '\n');
+	do { cin.get(c); } while (c != '\n' && c != EOF);
+	return;
+}
